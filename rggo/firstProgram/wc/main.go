cdb023bead258176127744d17b3d69e569e9ae21
rinarudhei <rinaldiadrian5@gmail.com>
2025-05-18 16:55:48 +0700

    - add option to read files - handle multiple files ex: "./wc -f file1.txt file2.txt ..."

--- rggo/firstProgram/wc/main.go
+++ rggo/firstProgram/wc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -9,20 +10,39 @@ import (
 )
 
 func main() {
-	lines := flag.Bool("l", false, "count lines")
-	bytes := flag.Bool("b", false, "count bytes")
+	countLines := flag.Bool("l", false, "count lines")
+	countBytes := flag.Bool("b", false, "count bytes")
+	file := flag.Bool("f", false, "scan text file")
 
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines, *bytes))
+	var r io.Reader
+	if *file {
+		filenames := flag.Args()
+		joinedByte := []byte{}
+		for _, f := range filenames {
+			b, err := os.ReadFile(f)
+			if err != nil {
+				fmt.Printf("Error reading a file: %s", f)
+				os.Exit(1)
+			}
+			joinedByte = append(joinedByte, b...)
+		}
+
+		r = bytes.NewReader(joinedByte)
+	} else {
+		r = os.Stdin
+	}
+
+	fmt.Println(count(r, *countLines, *countBytes))
 }
 
 func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
-	if !countLines && !countBytes{
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
 	} else if countBytes {
-        scanner.Split(bufio.ScanBytes)
-    }
+		scanner.Split(bufio.ScanBytes)
+	}
 
 	wc := 0
 	for scanner.Scan() {
